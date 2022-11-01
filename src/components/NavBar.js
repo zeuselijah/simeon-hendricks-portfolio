893cf8830215661a01ea071fa7775a858c36663d
zeuselijah <simeonhendricks@gmail.com>
2022-11-01 14:56:48 -0400

    wip: updating portfolio

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,31 @@
 import React from 'react'
 
 const NavBar = () => {
+    
   return (
     <nav className="nav">
         <a href="/" className='site-title'>
             Simeon Hendricks
         </a>
         <ul>
-            <li className="active">
-                <a href="https://docs.google.com/document/d/1-XvU8aV1EXUxV2jVYuuTL6U29cg7drTrqSKqM5OXV6A/edit" target="_blank" rel="noreferrer" >Resume</a>
-            </li>
-            <li className="active" smooth spy to="projects">
-                <a href="https://github.com/zeuselijah" target="_blank" rel="noreferrer" >Projects</a>
-            </li>
+            <CustomLink href="/about">About</CustomLink>
+            <CustomLink href="/skills">Skills</CustomLink>
+            <CustomLink href="/projects">Projects</CustomLink>
+            <CustomLink href="https://docs.google.com/document/d/1-XvU8aV1EXUxV2jVYuuTL6U29cg7drTrqSKqM5OXV6A/edit">Resume</CustomLink>
         </ul>
     </nav>
   )
 }
+
+function CustomLink ({ href, children, ...props }) {
+    const path = window.location.pathname
+
+    return (
+        <li className={path === href ? "active" : ""}>
+        <a href={href}>{children}</a>
+        </li>    
+    )
+}
+
 
 export default NavBar;
---
+++ src/pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './css/About.css';
+
+const About = () => {
+  return (
+    <div className='about-container'>
+        <div className='about-parent'>
+            <div className='about-details'>
+                <img src="https://i.imgur.com/ALeN6rl.jpg" alt="" className="headshot" />
+                <div className="container">
+                    <p>
+                        Certified Scrum Master and Software Engineer, with 10+ years of experience in 
+                        operations and project management. Solution-driven, agile, 
+                        lean six-sigma focused, team building and detail-oriented, 
+                        leading teams in solving client pain points. Proven to develop
+                        strong relationships across cultures and teams to provide leadership 
+                        in a fast-paced environment. 
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>  
+    
+  )
+}
+export default About;
